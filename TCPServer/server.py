9c9eac84d58e84fbf3a9f7f41945a1ec3db96fbf
R. Yin <916280067@qq.com>
2016-04-28 16:50:16 +0800

    ADD：save img

--- TCPServer/server.py
+++ TCPServer/server.py
@@ -7,13 +7,15 @@ import io
 import time,datetime
 import socket
 import random
-import image_classification_predict
+import image_classification_predict_back
+import Image
+from array import array
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 UPLOAD_FOLDER = 'uploads/'
-BUFFER_SIZE = 1024000
+BUFFER_SIZE = 1024000*10
 host = ''  
 port = 9999  
 addr = (host,port)  
@@ -25,7 +27,24 @@ def saveImg(imgString, address='no_address'):
     new_img_name = address + time_ + str(random.randint(10000, 1000000)) + '.jpg'
     with open(UPLOAD_FOLDER+new_img_name,'wb') as img:
         img.write(imgString)
-    return image_classification_predict.Main(UPLOAD_FOLDER+new_img_name)
+    return image_classification_predict_back.Main(UPLOAD_FOLDER+new_img_name)
+
+
+
+def byte2Img(imgString, address='no_address'):
+    if '<' in imgString and '>' in imgString:
+        imgString = imgString.replace('<','')
+        imgString = imgString.replace('>','')
+        time_ = str(datetime.datetime.now()).replace(' ', '_')
+        time_ = time_.replace(':','_')
+        address = address.replace('.','_')
+        new_img_name = address + time_ + str(random.randint(10000, 1000000)) + '.jpg'
+        with open(UPLOAD_FOLDER+new_img_name, 'w') as f:
+            f.write(imgString.replace(' ', '').decode('hex'))
+        return image_classification_predict_back.Main(UPLOAD_FOLDER+new_img_name)
+    else:
+    	return ''
+
 
 class Servers(SRH):  
     def handle(self):  
@@ -38,7 +57,7 @@ class Servers(SRH):
             print data  
             print "RECV from ", self.client_address[0]
             try:
-                msg = saveImg(data, self.client_address[0])
+                msg = byte2Img(data, self.client_address[0])
             except:
                 msg = 'error'
             self.request.send(msg)
--- test/image2byte/test.py
+++ test/image2byte/test.py
@@ -2,13 +2,4 @@
 with open("test.jpg", "rb") as imageFile:
   f = imageFile.read()
   b = bytearray(f)
-
-print b[2],type(b)
-import os
-import io
-import Image
-from array import array
-
-
-image = Image.open(io.BytesIO(b))
-image.save("test1.jpg")
+  print b,type(b)
